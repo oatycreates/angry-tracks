6a2815f1288834ad53842413081759daac2a1e79
Patrick Ferguson <patrick@ferguson.cloud>
2017-07-31 23:04:41 +1000

    Smooth out lerp of the camera in TitleScene.

---
+++ Assets/Scripts/Utility/LookAtLerp.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Author: Patrick Ferguson
+/// Date: 31st July 2017
+/// Look at target object with lerp
+/// </summary>
+
+public class LookAtLerp : MonoBehaviour
+{
+  public Transform target;
+
+  /// <summary>
+  /// Percentage to lerp the object rotation per second.
+  /// </summary>
+  public float lerpRate = 5.0f;
+
+  // Cached component references
+  private Transform m_transform = null;
+
+  void Start()
+  {
+    m_transform = GetComponent<Transform>();
+    if (!m_transform) {
+      Debug.LogError("Could not find Transform");
+    }
+  }
+
+  void Update()
+  {
+    if (target != null)
+    {
+      Quaternion newRot = Quaternion.LookRotation(target.position - m_transform.position, Vector3.up);
+      m_transform.rotation =
+        Quaternion.Slerp(m_transform.rotation, newRot, lerpRate * Time.deltaTime);
+    }
+  }
+
+}
